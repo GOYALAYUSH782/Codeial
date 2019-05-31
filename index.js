f03568c52a6e8fc5dee21950f4bc1ffb9e297e56
GOYALAYUSH782 <goyalayush782@gmail.com>
2019-05-31 17:18:15 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app=express();
+const port = 8000;
+const path = require('path');
+
+app.use('/',require('./routes'));
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`error in running the server: ${err}`);
+        return;
+    }
+    console.log(`server is running on port: ${port}`);
+});
