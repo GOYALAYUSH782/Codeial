86ded1a4b61a832c0586be4a6623c4a8976be227
GOYALAYUSH782 <goyalayush782@gmail.com>
2019-06-08 13:17:56 +0530

    add database mongoDB

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/codeial_development',);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
+
+db.once('open', function() {
+  console.log('connected to the database:: MongoDB')
+});
+
+module.exports=db;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app=express();
 const port = 8000;
-const path = require('path');
 const expresslayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+//const path = require('path');
 
 app.use(expresslayouts);
 app.set('layout extractStyles',true);
