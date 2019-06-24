54b87c8854a6bdc45a39b0af8ea569ee19decaa6
GOYALAYUSH782 <goyalayush782@gmail.com>
2019-06-24 19:01:38 +0530

    add async await to controllers

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,8 @@
 const Post = require('../models/post');
 const Comment=require('../models/comment');
 const User=require('../models/user');
-module.exports.home = function(req, res){
+
+module.exports.home = async function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
 
@@ -14,24 +15,26 @@ module.exports.home = function(req, res){
 
     // populate the user of each post
     
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user',
-        }
-    })
-    .exec(function(err, posts){
-        User.find({},(err,users)=>{
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users:users,
-            });
+    try{
+        let posts= await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user',
+            }
         })
-    })
-
+        let users = await User.find({});
+        
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users:users,
+        });
+    }catch(err){
+        console.log('Error',err);
+        return ;
+    }
 }
 
 // module.exports.actionName = function(req, res){}
